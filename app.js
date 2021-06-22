8198309299eab95594f7d53a54a29e3c23d78d9d
NatCulverhouse <77886577+NatCulverhouse@users.noreply.github.com>
2021-06-22 12:21:48 +0200

    Set Date

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const newDate = document.createElement("H1");
+const dateInput = document.querySelector(".input-date");
+
 const countdown = () => {
-    const countDate = new Date('june 1, 2021 00:00:00').getTime();
+    const countDate = new Date(dateInput.value).getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
+    
+    const H1Content = document.createTextNode(dateInput.value);
 
     const second = 1000;
     const minute = second * 60;
@@ -13,11 +18,13 @@ const countdown = () => {
     const textMinute = Math.floor(gap % hour / minute);
     const textSecond = Math.floor(gap % minute / second);
 
+    newDate.appendChild(H1Content);
+
     document.querySelector('.day').innerText = textDay;
     document.querySelector('.hour').innerText = textHour;
     document.querySelector('.minute').innerText = textMinute;
     document.querySelector('.second').innerText = textSecond;
-    document.querySelector('.date').innerText = new Date('june 1, 2021 00:00:00');
+    document.querySelector('.date').innerText = new Date(countDate);
     console.log(gap);
 };
 
